0846a286b1dffb3d727e3e28ad145b6f9537417a
jjmerelo <jjmerelo@19efad3a-db42-0410-b53b-377d24580092>
2008-01-04 09:06:00 +0000

    Quitado espacio

--- js/greasemonkey/add_tags.user.js
+++ js/greasemonkey/add_tags.user.js
@@ -12,7 +12,7 @@ function add_tags(s) {
   var tags = s.split(/,\s+/);
   var result='\n<span class="etiquetas_technorati">Etiquetas: ';
   for ( var i=0; i < tags.length; i++ ) {
-    result += "<a rel='tag' href='http://www.technorati.com/tag/"+tags[i]+"'>"+tags[i]+"</a> ";
+    result += "<a rel='tag' href='http://www.technorati.com/tag/"+tags[i]+"'>"+tags[i]+"</a>";
     if ( i < (tags.length-1) ) {
       result += ", ";
     }
